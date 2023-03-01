2f759a8370c2e1702042336260f43d4df290d206
lesmesrafa <58077392+lesmesrafa@users.noreply.github.com>
2023-03-01 20:05:48 +0100

    Latest versions updated

--- mushr_ros_ter/src/follow_path.py
+++ mushr_ros_ter/src/follow_path.py
@@ -10,7 +10,6 @@
 # ROS dependecies
 import rospy
 from nav_msgs.msg import Path
-from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped
 from tf.transformations import euler_from_quaternion
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
@@ -18,36 +17,16 @@ from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 # Python dependencies
 import math
 import numpy as np
-from statistics import mean
 
 # Global variables
 path = None
 current_pose = None
-angle_tolerance = 0.1 # Angle tolerance to consider the object as facing the target
-laser_distances = None
 
 
-def laser_callback(msg):
-    """Save current laser readings."""  
-    global laser_distances, regions, condition_left, condition_right, condition_front, condition_two_front_regions, front_left, front_right
-    # Values < range_min or > range_max should be discarded (10.0 meters).
-    # Nan values = max possible value (10.0 meters).
-    laser_distances = [round(distance, 2) if not math.isnan(distance) and (msg.range_min <= distance) and (msg.range_max >= distance) else 10.0 for distance in msg.ranges]
-    # Defining which parts of the LIDAR corresponds to a movement.
-    regions = {
-        "right": mean(laser_distances[99:279]), 
-        "front": mean(laser_distances[279:459]),
-        "left": mean(laser_distances[459:619]) 
-        }
-    # Define a threshold value for detecting obstacles
-    THRESHOLD = 7.0
-    condition_left = regions["left"] <= THRESHOLD
-    condition_right = regions["right"] <= THRESHOLD
-    condition_front = regions["front"] <= THRESHOLD
-    condition_two_front_regions = abs(mean(laser_distances[279:459][:80]) - mean(laser_distances[279:459][80:])) > 0.01 
-    
-    front_left = mean(laser_distances[279:459][80:])
-    front_right = mean(laser_distances[279:459][:80])
+def goal_callback(msg):
+    global target_pos
+    """Save desired destination."""
+    target_pos = (round(msg.pose.position.x, 1), round(msg.pose.position.y, 1))
 
 
 def path_callback(msg):
@@ -66,51 +45,17 @@ def current_pose_callback(msg):
     """Get the coordinates of the car."""
     global current_pose
     current_pose = msg
-    
-    
-def follow_path_logic(drive_publisher):
-    """Takes a drive_publisher argument and follows the given path."""
-    # Loop through each position in the path
-    for pos in path:
-        # Check if the distance to the target is greater than 0.1 units
-        if math.sqrt((pos[0] - current_pose.pose.position.x)**2 + (pos[1] - current_pose.pose.position.y)**2) > 0.1:
-            # Calculate the distance to the target
-            distance_to_object = math.sqrt((pos[0]- current_pose.pose.position.x)**2 + (pos[1] - current_pose.pose.position.y)**2)
-            # Calculate the angle to the target
-            relative_angle = math.atan2(pos[1] - current_pose.pose.position.y, pos[0] - current_pose.pose.position.x)        
-            # Get the current yaw angle of the car
-            yaw_car = euler_from_quaternion([current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z, current_pose.pose.orientation.w])[2]
-            # Calculate the difference between the target angle and the car's yaw angle
-            angle_diff = relative_angle - yaw_car
-            # Wrap the angle difference to [-pi, pi] range
-            angle_diff = math.atan2(np.sin(angle_diff), np.cos(angle_diff))
-            # If we are within 0.3 units of the target or we have reached the last point on the path      
-            if abs(distance_to_object) <= 0.3 or len(path) <= 1.0:
-                # Set the drive speed to 0 and the steering angle to 0
-                drive = AckermannDrive(speed=0.0, steering_angle=0.0)  
-            else:
-                # If the angle difference is less than -0.1, turn left
-                if angle_diff < -0.1:
-                    drive = AckermannDrive(speed=SPEED, steering_angle=-0.3)
-                # If the angle difference is greater than 0.1, turn right
-                elif angle_diff > 0.1:
-                    drive = AckermannDrive(speed=SPEED, steering_angle=0.3)
-                # Otherwise, drive straight
-                else:
-                    drive = AckermannDrive(speed=SPEED, steering_angle=0.0)
-             # Publish the drive command to the drive publisher   
-            drive_publisher.publish(AckermannDriveStamped(drive=drive))
 
 
 def follow_path():
     """Run all the logic to follow the path and to avoid obstacles."""
-    global path, current_pose, angle_tolerance, regions, condition_left, condition_right, condition_front, condition_two_front_regions, front_left, front_right, laser_distances
+    global path, current_pose
     # Define a publisher for the drive commands
     drive_publisher = rospy.Publisher('/car/mux/ackermann_cmd_mux/input/navigation', AckermannDriveStamped, queue_size=1)
     # Loop continuously while the ROS node is running
     while not rospy.is_shutdown():
         # Check if all required information is available   
-        if path is not None and current_pose is not None and laser_distances is not None:
+        if path is not None and current_pose is not None and len(path) > 0:
             # Calculate the relative angle of the car to the final point of the path
             relative_angle = math.atan2(path[-1][1] - current_pose.pose.position.y, path[-1][0] - current_pose.pose.position.x)        
             # Get the current yaw angle of the car
@@ -118,38 +63,32 @@ def follow_path():
             # Calculate the difference between the relative angle and the yaw angle
             angle_diff = relative_angle - yaw_car
             angle_diff = math.atan2(np.sin(angle_diff), np.cos(angle_diff))
-            # Check if there are no obstacles or only obstacles on one side
-            if (not condition_front and not condition_right and not condition_left) or (not condition_front and condition_right and condition_left) or (not condition_front and condition_left and angle_diff < 0.4) or (not condition_front and condition_right and angle_diff > -0.4):
-                # Follow the path while avoiding obstacles
-                follow_path_logic(drive_publisher)     
-            else:
-                # Avoid obstacles
-                drive = None
-                # Go straight if there are no obstacles in front or if there are only obstacles on the sides
-                if not condition_front or (not condition_front and condition_right and condition_left):
-                    drive = AckermannDrive(speed=SPEED, steering_angle=0.0)  
-                # Turn right if there are obstacles on the left 
-                elif condition_front and condition_left and not condition_right:
-                    drive = AckermannDrive(speed=SPEED, steering_angle=-0.3) 
-                # Turn left if there are obstacles on the right
-                elif condition_front and condition_right and not condition_left:
-                    drive = AckermannDrive(speed=SPEED, steering_angle=0.3) 
-                # Turn left or right if there are no obstacles in front but obstacles on both sides
-                elif condition_front and not condition_left and not condition_right:
-                    if regions["left"] > regions["right"]:
-                        drive = AckermannDrive(speed=SPEED, steering_angle=0.3) # Go left
-                    else: 
-                        drive = AckermannDrive(speed=SPEED, steering_angle=-0.3) # Go right
-                # Turn left or right if there are multiple obstacles in front      
-                elif condition_two_front_regions:
-                    if front_left > front_right:
-                        drive = AckermannDrive(speed=SPEED, steering_angle=0.3) # Go left
-                    else: 
-                        drive = AckermannDrive(speed=SPEED, steering_angle=-0.3) # Go right
-                # Stop if there are obstacles
+            # Loop through each position in the path
+            for pos in path[1:]:   
+                distance_to_target = math.sqrt((target_pos[0]- current_pose.pose.position.x)**2 + (target_pos[1] - current_pose.pose.position.y)**2)
+                # Calculate the angle to the target
+                relative_angle = math.atan2(pos[1] - current_pose.pose.position.y, pos[0] - current_pose.pose.position.x)        
+                # Get the current yaw angle of the car
+                yaw_car = euler_from_quaternion([current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z, current_pose.pose.orientation.w])[2]
+                # Calculate the difference between the target angle and the car's yaw angle
+                angle_diff = relative_angle - yaw_car
+                # Wrap the angle difference to [-pi, pi] range
+                angle_diff = math.atan2(np.sin(angle_diff), np.cos(angle_diff))
+                # If we are within 0.3 units of the target or we have reached the last point on the path      
+                if abs(distance_to_target) <= 0.3 or pos is path[-1]:
+                    # Set the drive speed to 0 and the steering angle to 0
+                    drive = AckermannDrive(speed=0.0, steering_angle=0.0)  
                 else:
-                    drive = AckermannDrive(speed=0.0, steering_angle=0.0) 
-
+                    # If the angle difference is less than -0.1, turn left
+                    if angle_diff < -0.1:
+                        drive = AckermannDrive(speed=SPEED, steering_angle=-0.3)
+                    # If the angle difference is greater than 0.1, turn right
+                    elif angle_diff > 0.1:
+                        drive = AckermannDrive(speed=SPEED, steering_angle=0.3)
+                    # Otherwise, drive straight
+                    else:
+                        drive = AckermannDrive(speed=SPEED, steering_angle=0.0)
+                # Publish the drive command to the drive publisher   
                 drive_publisher.publish(AckermannDriveStamped(drive=drive))
                 
 
@@ -157,10 +96,10 @@ def follow_path():
 if __name__ == '__main__':
     # Initializes a ROS node with the name 'follow_path_node'
     rospy.init_node('follow_path_node')
-    # Subscribes to the topics 'path_to_follow', 'car_pose', and 'car/scan'
+    # Subscribes to the topics 'path_to_follow', 'car_pose', and '/move_base_simple/goal'
     rospy.Subscriber('/path_to_follow', Path, path_callback)
     rospy.Subscriber('/car/car_pose', PoseStamped, current_pose_callback)
-    rospy.Subscriber('/car/scan', LaserScan, laser_callback)
+    rospy.Subscriber('/move_base_simple/goal', PoseStamped, goal_callback)
     # Retrieves the 'speed' parameter from the ROS parameter server
     SPEED = float(rospy.get_param("~speed"))
     # Calls the 'follow_path' function
--- mushr_ros_ter/src/teb_planner.py
+++ mushr_ros_ter/src/teb_planner.py
@@ -10,7 +10,7 @@
 # ROS dependecies
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped
-from nav_msgs.msg import OccupancyGrid, Path
+from nav_msgs.msg import Path
 from tf.transformations import euler_from_quaternion
 
 # Python dependencies
@@ -23,11 +23,9 @@ class TimeElasticBandController:
     def __init__(self):
         # Initialize ROS node
         rospy.init_node('time_elastic_band_controller')
-        # Set up map and occupancy grid
-        self.origin = None
-        self.width = None
-        self.height = None
-        self.resolution = None
+        self.min_threshold = 0.1
+        self.max_scope_laser = 9.0
+        self.speed = float(rospy.get_param("~speed"))
         # Laser configuration
         self.laser_start_angle = None
         self.laser_angle_increment = None
@@ -38,9 +36,20 @@ class TimeElasticBandController:
         self.yaw = None
         # Set up desired destination
         self.target_pos = None
+        self.old_target_pos = None
         # Obstacles
         self.obstacles = None
-
+        self.min_dist_object = 1.0
+        
+        
+        self.obstacles_front = None
+        self.obstacles_right = None
+        self.obstacles_left = None
+        
+        
+        
+        # self.flag = False
+        self.path_saved = []
    
         # Publishers
         self.publish_path = rospy.Publisher("/path_to_follow", Path, queue_size=1)
@@ -48,7 +57,6 @@ class TimeElasticBandController:
         self.sub_car_pose = rospy.Subscriber('/car/car_pose', PoseStamped, self.odom_callback)
         self.sub_car_scan = rospy.Subscriber('/car/scan', LaserScan, self.laser_callback)
         self.sub_goal = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.goal_callback)
-        self.sub_map = rospy.Subscriber('/map', OccupancyGrid, self.map_callback)
     
     
     ##############################################################################
@@ -90,15 +98,7 @@ class TimeElasticBandController:
         # Save laser start angle and increment
         self.laser_start_angle = msg.angle_min
         self.laser_angle_increment = msg.angle_increment
-        
-    
-    def map_callback(self, msg):
-        """Save map data and metadata."""
-        self.origin = (msg.info.origin.position.x, msg.info.origin.position.y)
-        self.width = msg.info.width
-        self.height = msg.info.height
-        self.resolution = msg.info.resolution
-        
+
         
     def goal_callback(self, msg):
         """Save desired destination."""
@@ -113,21 +113,18 @@ class TimeElasticBandController:
     def calculate_coordinates(self, index, reading):
         """Convert laser reading index and distance to map coordinates."""
         # Calculate map coordinates using trigonometry and current position of the robot
-        angle = self.laser_start_angle + self.laser_angle_increment * index
+        angle = self.laser_start_angle + self.laser_angle_increment * index + self.yaw
         x = reading * math.cos(angle) + self.current_pos[0]
         y = reading * math.sin(angle) + self.current_pos[1]
+
         return x, y
-    
-    
-    def update_map(self):
+
+
+    def get_obstacles(self):
         """Update map data with current laser readings.""" 
         # Checking if laser scan data is not None
         if self.laser_readings is None:
             rospy.logwarn("Laser scan is not yet initialized, skipping updating map.")
-            return
-        # Checking if map data is not None
-        if self.origin is None: 
-            rospy.logwarn("Map is not yet initialized, skipping updating map.")
             return
         # Getting obstacles
         self.obstacles = []
@@ -140,92 +137,208 @@ class TimeElasticBandController:
         
     
     ##############################################################################
-    ############################# D* Lite Algorithm ##############################
+    ############################# TEB Controller Algorithm #######################
     ##############################################################################
+
+
+    def min_distance(self, points1, points2):
+        """Returns the minimum distance between any point in points1 and any point in points2."""
+        min_dist = math.inf
+        for p1 in points1:
+            for p2 in points2:
+                dist = ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
+                if dist < min_dist:
+                    min_dist = dist
+        return min_dist
     
     
     def get_neighbors(self, point):
-        """Calculate neighbors of 'point'.""" 
-        x, y = point
-        return [(x-0.1, y-0.1), (x-0.1, y), (x-0.1, y+0.1), (x, y-0.1), (x, y+0.1), (x+0.1, y-0.1), (x+0.1, y), (x+0.1, y+0.1)]
+            """Calculate neighbors points of 'point'.""" 
+            x, y = point
+            growing_factor = 0.1
+            neighbors = [(round(x-growing_factor,1), round(y-growing_factor,1)), (round(x-growing_factor,1), y), (round(x-growing_factor,1), round(y+growing_factor,1)), (x, round(y-growing_factor,1)), (x, round(y+growing_factor,1)), (round(x+growing_factor,1), round(y-growing_factor,1)), (round(x+growing_factor,1), y), (round(x+growing_factor,1), round(y+growing_factor,1))]
+            return neighbors
 
 
     def calculate_cost(self, point, target):
-        """Calculate cost to go from 'point' to target.""" 
-        if not isinstance(target, list):
-            return abs(np.sqrt((target[0]-point[0])**2 + (target[1]-point[1])**2))
+            """Calculate cost to go from 'point' to target.""" 
+            if not isinstance(target, list):
+                return abs(np.sqrt((target[0]-point[0])**2 + (target[1]-point[1])**2))
+            
+
+    def check_distance(self, point, points):
+        """Calculate distance from 'point' to the closest point of 'points' list."""
+        a, b = point
+        min_dist = 999999
+        for p in points:
+            dist = math.sqrt((p[0] - a)**2 + (p[1] - b)**2)
+            if dist < min_dist:
+                min_dist = dist
+        return min_dist
        
+       
+    def rdp(self, path, epsilon):
+        """
+        Simplifies a path using the Ramer-Douglas-Peucker algorithm.
+        """
+        def point_line_distance(point, start, end):
+            """
+            Calculates the distance between a point and a line segment.
+            """
+            if np.all(np.equal(start, end)):
+                return np.linalg.norm(np.array(point) - np.array(start))
+
+            l2 = np.linalg.norm(np.array(end) - np.array(start)) ** 2
+            t = max(0, min(1, np.dot(np.array(point) - np.array(start), np.array(end) - np.array(start)) / l2))
+            projection = np.array(start) + t * (np.array(end) - np.array(start))
+            return np.linalg.norm(np.array(point) - projection)
+        dmax = 0
+        index = 0
+        for i in range(1, len(path) - 1):
+            d = point_line_distance(path[i], path[0], path[-1])
+            if d > dmax:
+                index = i
+                dmax = d
+        if dmax > epsilon:
+            results = self.rdp(path[:index + 1], epsilon)[:-1] + self.rdp(path[index:], epsilon)
+        else:
+            results = [path[0], path[-1]]
+
+        return results
+    
+    
+    def smooth_path(self, path, min_distance=1):
+        """
+        Smooth path generated by Ramer-Douglas-Peucker algorithm.
+        """
+        new_path = [path[0]] # initialize new path with first point
+    
+        for i in range(1, len(path)-1):
+            x0, y0 = path[i-1]
+            x1, y1 = path[i]
+            x2, y2 = path[i+1]
+            dist = math.hypot(x2-x0, y2-y0) # distance between i-1 and i+1
+            
+            if dist > min_distance:
+                new_path.append(path[i])
+            else:
+                dist1 = math.hypot(x1-x0, y1-y0) # distance between i-1 and i
+                dist2 = math.hypot(x2-x1, y2-y1) # distance between i and i+1
+                dist3 = math.hypot(x2-x0, y2-y0) # distance between i-1 and i+1
+                
+                if dist1 <= dist2 and dist1 <= dist3:
+                    new_path[-1] = path[i] # replace last point with current point
+                elif dist2 <= dist3:
+                    pass # don't add current point to new path
+                else:
+                    new_path.append(path[i+1]) # add next point to new path
+                
+        new_path.append(path[-1]) # add last point to new path
+        return new_path
+    
         
-    def d_star_lite(self):
-        """Calculate shortest path to target impleemnting D* Lite algorithm.""" 
+    def teb_path_generation(self):
+        """Calculate shortest path to target.""" 
         # Check if odometry is not None
         if self.xy_pos is None: 
-            rospy.logwarn("Odometry is not yet initialized, skipping D* lite.")
+            rospy.logwarn("Odometry is not yet initialized, skipping path creation.")
             return
         # Check if target is not None
         if self.target_pos is None: 
-            rospy.logwarn("Target point is not yet initialized, skipping D* lite.")
+            rospy.logwarn("Target point is not yet initialized, path creation.")
             return
         # Check if laser is not None
         if self.obstacles is None: 
-            rospy.logwarn("Laser is not yet initialized, skipping D* lite.")
+            rospy.logwarn("Laser is not yet initialized, path creation.")
             return
         # Initialize start, end and obstacles
-        start = self.xy_pos
+        start = (self.xy_pos[0] + 1, self.xy_pos[1] + 1)
         end = self.target_pos
         obstacles = self.obstacles
-            
+        
         visited = {start:self.calculate_cost(start, end)}
+      
         path = [start]
+       
+        current_node = None
         # While the visited list is not empty
         while len(visited) != 0:
+            if len(path) == 1:
+                current_node = start
             # Get the node with the minimum cost
-            current_node = min(visited, key=visited.get)
             current_cost = visited[current_node]
             # If the cost is below a threshold, return the path
-            if current_cost < 0.1:
+            if current_cost <= 0.1 or len(path) == 400*self.speed:
+                self.path_saved = path
                 return path
-            # Remove the node from the visited list
-            del visited[current_node]
             # Get the neighbors of the current node and their costs
             neighbors = self.get_neighbors(current_node)
             set_neighbors = {}
             # For each neighbor, calculate its cost and add it to a set
             for neighbor in neighbors:
-                if neighbor in obstacles: 
+                if neighbor in obstacles or neighbor in visited.keys():
                     continue
+                if len(obstacles) > 5:
+                    if self.check_distance(neighbor, obstacles) <= self.min_dist_object:
+                        
+                        continue
+
                 set_neighbors[neighbor] = self.calculate_cost(neighbor, end)
             # Get the neighbor with the minimum cost and add it to the path 
-            future_node = min(set_neighbors, key=set_neighbors.get)
-            visited[future_node] = set_neighbors[future_node]
-            path.append(future_node)
-            
-    
+            if len(set_neighbors) == 0:
+                break
+            else:
+                current_node = min(set_neighbors, key=set_neighbors.get)
+                visited[current_node] = set_neighbors[current_node]
+                path.append(current_node)
+
     ##############################################################################
     ################################ Run the node ################################
     ##############################################################################
-    
-    
+
     def run(self):
         """Main loop."""
         rate = rospy.Rate(10)
         # Continuously update the map and calculate the path
+        cont = 0 
         while not rospy.is_shutdown():
-            self.update_map()
-            path = self.d_star_lite()
+            self.get_obstacles()
+            path = self.teb_path_generation()
+           
             # If a path is found, publish it as a path message
             if path is not None:
-                path_msg = Path()
-                path_msg.header.frame_id = "map"
-                # Iterate through each point in the path, and append it to the path message
-                for i, (x, y) in enumerate(path):
-                    pose = PoseStamped()
-                    pose.header.frame_id = "map"
-                    pose.header.seq = i
-                    pose.pose.position.x = x
-                    pose.pose.position.y = y
-                    path_msg.poses.append(pose)
-                self.publish_path.publish(path_msg)
+                if self.target_pos != self.old_target_pos or cont == 0:
+                    self.old_target_pos = self.target_pos 
+                    cont = 0
+                    
+
+                if cont == 0:
+                    cont += 1
+                    path = path#self.smooth_path(self.rdp(path, epsilon=200.0))
+                    path_msg = Path()
+                    path_msg.header.frame_id = "map"
+                    for i, (x, y) in enumerate(path):
+                        pose = PoseStamped()
+                        pose.header.frame_id = "map"
+                        pose.header.seq = i
+                        pose.pose.position.x = x
+                        pose.pose.position.y = y
+                        path_msg.poses.append(pose)
+                    self.publish_path.publish(path_msg)
+                    
+                else:
+                    path_msg = Path()
+                    path_msg.header.frame_id = "map"
+                    for i, (x, y) in enumerate(path):
+                        pose = PoseStamped()
+                        pose.header.frame_id = "map"
+                        pose.header.seq = i
+                        pose.pose.position.x = x
+                        pose.pose.position.y = y
+                        path_msg.poses.append(pose) 
+                    self.publish_path.publish(path_msg)
+
+
             rate.sleep()
 
 
